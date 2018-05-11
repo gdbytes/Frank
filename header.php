662cfdca2c385a2c8eb5a0034b51d2a3443ae558
Michael Forcer <michael@goodbytes.co.uk>
2018-05-11 12:52:17 +0100

    Modernised header.php - removed old code

--- header.php
+++ header.php
@@ -1,52 +1,31 @@
-<?php
-/**
- * @package Frank
- */
-?>
 <!DOCTYPE html>
-<!--[if IE 7 | IE 8]>
-<html class="ie" lang="en-US">
-<![endif]-->
-<!--[if (gte IE 9) | !(IE)]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-
-	<title>
-		<?php
-		wp_title( '&mdash;', true, 'right' );
-		bloginfo( 'name' );
-		?>
-	</title>
-
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
-<body id="page" <?php body_class(); ?>>
-	<!--[if lt IE 9]>
-		<div class="chromeframe">Your browser is out of date. Please <a href="http://browsehappy.com/">upgrade your browser </a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a>.</div>
-	<![endif]-->
-<div class="container">
-	<header id="page-header" class="row">
+<body <?php body_class(); ?>>
 
-        <h1 id="site-title"><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-        <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-        <?php the_header_image_tag(); ?>
+    <div id="page" class="container">
 
-		<nav id="site-nav">
-			<?php if ( !dynamic_sidebar( 'Navigation' ) ) : ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'frank_primary_navigation', 'container' => false ) ); ?>
-			<?php endif; ?>
-		</nav>
-		<?php if ( is_active_sidebar( 'widget-subheader' ) ) : ?>
-		<div id='sub-header' class='row'>
-			<?php if ( !dynamic_sidebar( 'Sub Header' ) ) : ?>
-			<?php endif; ?>
-		</div>
-		<?php endif; ?>
-	</header>
+	    <header id="masthead" class="row">
+
+            <h1 id="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+            <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+            <?php the_header_image_tag(); ?>
+
+            <nav id="site-nav">
+                <?php if ( !dynamic_sidebar( 'Navigation' ) ) : ?>
+                    <?php wp_nav_menu( array( 'theme_location' => 'frank_primary_navigation', 'container' => false ) ); ?>
+                <?php endif; ?>
+            </nav>
+
+            <?php if ( is_active_sidebar( 'widget-subheader' ) ) : ?>
+                <div id='sub-header' class='row'>
+                    <?php if ( !dynamic_sidebar( 'Sub Header' ) ) : ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+	    </header>
