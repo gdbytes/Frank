1c144cbfcdaa9c954bf70abe5955efa169a6d129
Michael Forcer <michael@goodbytes.co.uk>
2018-05-11 12:53:52 +0100

    Cleaned up footer.php

--- footer.php
+++ footer.php
@@ -1,20 +1,15 @@
-<?php
-/**
- * @package Frank
- */
-?>
-</div>
-<?php if ( is_active_sidebar( 'widget-footer' ) ) : ?>
-<div id="page-bottom">
-	<footer id="page-footer" class="container" role="contentinfo">
-		<div class="row">
-		<?php if ( ! dynamic_sidebar( 'Footer' ) ) : ?>
+    </div>
+    <?php if ( is_active_sidebar( 'widget-footer' ) ) : ?>
+        <div id="page-bottom">
+            <footer id="page-footer" class="container" role="contentinfo">
+                <div class="row">
+                    <?php if ( ! dynamic_sidebar( 'Footer' ) ) : ?>
+                    <?php endif; ?>
+                </div>
+            </footer>
+        </div>
+    <?php endif; ?>
 
-		<?php endif; ?>
-		</div>
-	</footer>
-</div>
-<?php endif; ?>
 <?php wp_footer(); ?>
 </body>
 </html>
